6c9b6a39c2f769d4f82f302ec18e3b842c846a31
jwellington <jwellington@ptc.com>
2011-04-19 10:01:43 -0500

    Initial Commit

---
+++ Influence Maps/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Influence_Maps
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.nodeGrid = new System.Windows.Forms.DataGridView();
+            this.X = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Y = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label7 = new System.Windows.Forms.Label();
+            this.statusBox = new System.Windows.Forms.RichTextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(243, 556);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Generate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(573, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Green = Start/End";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(573, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Orange = Path";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(573, 77);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Purple = Opened Node";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(576, 94);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Yellow = On Queue";
+            // 
+            // nodeGrid
+            // 
+            this.nodeGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.nodeGrid.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.X,
+            this.Y});
+            this.nodeGrid.Location = new System.Drawing.Point(540, 147);
+            this.nodeGrid.Name = "nodeGrid";
+            this.nodeGrid.Size = new System.Drawing.Size(239, 242);
+            this.nodeGrid.TabIndex = 11;
+            // 
+            // X
+            // 
+            this.X.HeaderText = "X";
+            this.X.Name = "X";
+            // 
+            // Y
+            // 
+            this.Y.HeaderText = "Y";
+            this.Y.Name = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(540, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Points:";
+            // 
+            // statusBox
+            // 
+            this.statusBox.Location = new System.Drawing.Point(543, 420);
+            this.statusBox.Name = "statusBox";
+            this.statusBox.Size = new System.Drawing.Size(236, 170);
+            this.statusBox.TabIndex = 13;
+            this.statusBox.Text = "";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(543, 401);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Status:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 673);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.statusBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.nodeGrid);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nodeGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView nodeGrid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn X;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Y;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox statusBox;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ Influence Maps/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Influence_Maps
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Dictionary<int, int> nodeList;
+        Graphics g;
+        const int WIDTH = 50;
+        const int HEIGHT = WIDTH;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            g = this.CreateGraphics();
+            g.Clear(System.Drawing.Color.White);
+            Pen pen = new Pen(System.Drawing.Color.Red);
+
+             nodeList = new Dictionary<int, int>();
+
+            for (int i = 0; i <= WIDTH * 10; i = i + 10)
+            {
+                g.DrawLine(pen, i, 0, i, WIDTH * 10);
+                g.DrawLine(pen, 0, i, HEIGHT * 10, i);
+                for (int j = 0; j < HEIGHT * 10; j = j + 10)
+                    nodeList[toPoint(i, j)] = 0;
+            }
+
+            int xtemp;
+            int ytemp;
+            int startPoint;
+            List<int> goalNodes = new List<int>();
+            foreach (DataGridViewRow row in nodeGrid.Rows)
+            {
+                if (row.Cells[0].Value != null && row.Cells[0] != null)
+                {
+                    if (row.Cells[0].Value != null && int.TryParse(row.Cells[0].Value.ToString(), out xtemp) && int.TryParse(row.Cells[1].Value.ToString(), out ytemp) && xtemp < WIDTH && xtemp >= 0 && ytemp < HEIGHT && ytemp >= 0)
+                    {
+                        startPoint = toPoint(xtemp * 10, ytemp * 10);
+                        goalNodes.Add(startPoint);
+                    }
+                    else
+                    {
+                        statusWrite("Point (" + row.Cells[0].Value.ToString() + ", " + row.Cells[1].Value.ToString() + ") does not exist");
+                    }
+                }
+            }
+
+            for (int i = 0; i < goalNodes.Count; i++)
+            {
+                for (int j = i+1; j < goalNodes.Count; j++)
+                {
+                    findPath(goalNodes[i], goalNodes[j]);
+                    //statusWrite("from: " + i + "\tto: " + j);
+                }
+            }
+
+            for (int i = 0; i < goalNodes.Count; i++)
+            {
+                nodeList[goalNodes[i]] = -2;
+            }
+
+            fillNodes();
+            
+        }
+
+        private int toPoint(int i, int j)
+        {
+            return i * 1000 + j;
+        }
+
+        private void statusWrite(string str)
+        {
+            statusBox.Text = statusBox.Text + str + "\n";
+        }
+
+
+        private void findPath(int startPoint, int goalPoint)
+        {
+            Queue<int> q = new Queue<int>();
+            Dictionary<int, int> parent = new Dictionary<int, int>();
+            q.Enqueue(startPoint);
+            int nextPoint = q.Dequeue();
+            parent[nextPoint] = nextPoint;
+            while (goalPoint != nextPoint)
+            {
+                if ((!parent.ContainsKey(nextPoint + toPoint(-10, 0))) && nodeList.ContainsKey(nextPoint + toPoint(-10, 0)))
+                {
+                    parent[nextPoint + toPoint(-10, 0)] = nextPoint;
+                    q.Enqueue(nextPoint + toPoint(-10, 0));
+                }
+                if ((!parent.ContainsKey(nextPoint + toPoint(0, -10))) && nodeList.ContainsKey(nextPoint + toPoint(0, -10)))
+                {
+                    parent[nextPoint + toPoint(0, -10)] = nextPoint;
+                    q.Enqueue(nextPoint + toPoint(0, -10));
+                }
+                if ((!parent.ContainsKey(nextPoint + toPoint(0, 10))) && nodeList.ContainsKey(nextPoint + toPoint(0, 10)))
+                {
+                    parent[nextPoint + toPoint(0, 10)] = nextPoint;
+                    q.Enqueue(nextPoint + toPoint(0, 10));
+                }
+                if ((!parent.ContainsKey(nextPoint + toPoint(10, 0))) && nodeList.ContainsKey(nextPoint + toPoint(10, 0)))
+                {
+                    parent[nextPoint + toPoint(10, 0)] = nextPoint;
+                    q.Enqueue(nextPoint + toPoint(10, 0));
+                }
+                nextPoint = q.Dequeue();
+                //nodeList[nextPoint] = -4;
+            }
+
+            nextPoint = parent[goalPoint];
+            while (nextPoint != startPoint)
+            {
+                nodeList[nextPoint] = -5;
+                nextPoint = parent[nextPoint];
+            }
+        }
+
+
+        private void fillNodes()
+        {
+            Brush blueBrush = new SolidBrush(System.Drawing.Color.Blue);
+            Brush greenBrush = new SolidBrush(System.Drawing.Color.Green);
+            Brush yellowBrush = new SolidBrush(System.Drawing.Color.Yellow);
+            Brush purpleBrush = new SolidBrush(System.Drawing.Color.Purple);
+            Brush orangeBrush = new SolidBrush(System.Drawing.Color.Orange);
+
+            for (int x = 0; x < WIDTH * 10; x = x + 10)
+            {
+                for (int y = 0; y < HEIGHT * 10; y = y + 10)
+                {
+                    Rectangle toFill = new Rectangle(x + 1, y + 1, 9, 9);
+                    switch (nodeList[toPoint(x, y)])
+                    {
+                        
+                        case -1:
+                            g.FillRectangle(blueBrush, toFill);
+                            break;
+                        case -2:
+                            g.FillRectangle(greenBrush, toFill);
+                            break;
+                        case -3:
+                            //g.FillRectangle(yellowBrush, toFill);
+                            break;
+                        case -4:
+                            //g.FillRectangle(purpleBrush, toFill);
+                            break;
+                        case -5:
+                            g.FillRectangle(orangeBrush, toFill);
+                            break;
+                    }
+
+                }
+            }
+        }
+
+
+    }
+
+    
+}
